cd0d6dbfa766f0a4a9bf405eb64ba180e294a70a
Asem Gadu <81575109+Asm25xr@users.noreply.github.com>
2023-05-25 23:59:49 +0300

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,12 +10,16 @@
 
 void print_square(int size)
 {
-	int row, column;
-
-	for (row = 1; row <= size; ++row)
+	if (size <= 0)
+		_putchar('\n'); 
+	else
 	{
-		for (column = 1; column <= size; ++column)
-			_putchar('#');
-		_putchar('\n');
+		int i, j;
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
 	}
 }
